31026dba3f5a35fba71cc75c7b0ad6e1dbe95515
vonhatphuongahihi <vonhatphuongahihi@gmail.com>
2024-04-06 19:07:57 +0700

    update1

--- app/src/main/java/com/example/movieapp/Activities/SignUp.kt
+++ app/src/main/java/com/example/movieapp/Activities/SignUp.kt
@@ -7,12 +7,12 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.movieapp.R
 
-class SignUp : AppCompatActivity() {
+class activity_sign_up : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_sign_up)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.signup)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
