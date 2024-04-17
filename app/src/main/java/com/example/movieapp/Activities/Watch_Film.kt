a3aea9ccb4a36f0b0a2ebba8f57ff42727ea5c2f
vonhatphuongahihi <vonhatphuongahihi@gmail.com>
2024-04-17 13:35:29 +0700

    update_watch_film

---
+++ app/src/main/java/com/example/movieapp/Activities/Watch_Film.kt
@@ -0,0 +1,21 @@
+package com.example.movieapp.Activities
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.movieapp.R
+
+class Watch_Film : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_watch_film)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
