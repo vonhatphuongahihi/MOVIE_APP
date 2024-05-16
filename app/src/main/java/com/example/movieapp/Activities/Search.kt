1a6e62dedff4c7e45b50dc039be47958ac9e87d4
vonhatphuongahihi <vonhatphuongahihi@gmail.com>
2024-05-17 00:51:12 +0700

    update_hihi

--- app/src/main/java/com/example/movieapp/Activities/Search.kt
+++ app/src/main/java/com/example/movieapp/Activities/Search.kt
@@ -5,17 +5,25 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.movieapp.R
 
 class Search : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_search)
+        setContentView(R.layout.activity_search_screen)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.search)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val recyclerView: RecyclerView = findViewById(R.id.recyclerView_trend)
+        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        val trendImages = listOf(R.drawable.khanh_khanh_trend, R.drawable.tien_kiem_4_trend, R.drawable.so_kieu_trend, R.drawable.truong_phong_do_trend)
+        val adapter = TrendAdapter(this, trendImages)
+        recyclerView.adapter = adapter
     }
 }
